859e975aa15d33507cf0ede623c590372cfd800c
Roberto A. Zavala <112892402+QuiahEditorial@users.noreply.github.com>
2022-11-19 18:59:27 -0600

    Update LM339.ino

--- LM339.ino
+++ LM339.ino
@@ -1,3 +1,8 @@
+// Por Roberto A. Zavala
+// Breve : https://es.overleaf.com/read/ksspyfprvykc
+// Libro : https://www.amazon.com.mx/dp/B074TTGLL2
+// 🙏🏼    : DNv7acPAeVBhTXbKv26itJecPG1SPy2o4F
+
 int A;
 int B;
 
